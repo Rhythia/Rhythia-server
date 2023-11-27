00ef289934d836b51d83490bb00aa19871e5cb58
jayden <46307433+krmeet@users.noreply.github.com>
2023-11-27 21:37:40 +0000

    add sync data

--- Rhythia.Game/Online/Spectator/StreamData.cs
+++ Rhythia.Game/Online/Spectator/StreamData.cs
@@ -5,5 +5,6 @@ namespace Rhythia.Game.Online.Spectator;
 [MessagePackObject]
 public class StreamData
 {
-    [Key(0)] public Array Frames { get; set; } // TODO: Assign replay frame type to this array
+    [Key(0)] public StreamSyncData SyncData { get; set; }
+    [Key(1)] public Array Frames { get; set; } // TODO: Assign replay frame type to this array
 }
--- Rhythia.Game/Online/Spectator/StreamInfo.cs
+++ Rhythia.Game/Online/Spectator/StreamInfo.cs
@@ -6,4 +6,5 @@ namespace Rhythia.Game.Online.Spectator;
 public class StreamInfo
 {
     [Key(0)] public string? MapId { get; set; } // This is NOT the id of the mapset
+    [Key(1)] public StreamSyncData SyncData { get; set; }
 }
